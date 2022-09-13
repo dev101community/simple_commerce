5c5c1f4c13787ef6a94f39eb061e0651d45dda7c
muthu <muthupandianthangarajan@gmail.com>
2022-09-14 00:41:51 +0530

    Calculating total

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -9,23 +9,32 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductComponent implements OnInit {
   data: any;
+  total_value: number = 0;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<any>('http://localhost:5000/product').subscribe(data => {
         this.data = data;
+        this.total_value = 0;
     }) 
   }
 
-  reduceQty(data: number): void {
-    const currentValue = Number((<HTMLInputElement>document.getElementById("qty-"+data)).value);
-    (<HTMLInputElement>document.getElementById("qty-"+data)).value = String(currentValue - 1);
+  reduceQty(data: number, product: any): void {
+    let currentValue = Number((<HTMLInputElement>document.getElementById("qty-"+data)).value) - 1;
+    if (currentValue < 0) {
+      currentValue = 0
+    }
+    this.total_value = this.total_value - product.fields.price;
+    alert(this.total_value);
+    (<HTMLInputElement>document.getElementById("qty-"+data)).value = String(currentValue);
   }
 
-  increaseQty(data: number): void {
-    const currentValue = Number((<HTMLInputElement>document.getElementById("qty-"+data)).value);
-    (<HTMLInputElement>document.getElementById("qty-"+data)).value = String(currentValue + 1);
+  increaseQty(data: number, product: any): void {
+    const currentValue = Number((<HTMLInputElement>document.getElementById("qty-"+data)).value) + 1;
+    this.total_value = this.total_value + product.fields.price;
+    alert(this.total_value);
+    (<HTMLInputElement>document.getElementById("qty-"+data)).value = String(currentValue);
   }
 
 }
